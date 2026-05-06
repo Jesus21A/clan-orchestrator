787c264a04985a9aaee6f645c849201f3803b85d
Jesus21A <jesuszapataromero@gmail.com>
2026-05-06 08:33:55 -0500

    correccion

--- frontend/src/environments/environment.production.ts
+++ frontend/src/environments/environment.production.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'clan-orchestrator-production.up.railway.app'
+  apiUrl: 'https://clan-orchestrator-production.up.railway.app/api/v1'
 };
